e1aef430ed5f1828ef2cb28522b86b6c75799c01
Udgeet <bhatt8udgeet@gmail.com>
2024-08-25 01:58:02 +0530

    Folders Setup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return ( 
+    <div>
+      This is Login Page!!
+    </div>
+   );
+}
+ 
+export default Login;
---
+++ app/(auth)/(routes)/resgister/page.tsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return ( 
+    <div>
+      This is Register Page!!
+    </div>
+   );
+}
+ 
+export default Register;
