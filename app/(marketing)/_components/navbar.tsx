a8f9a03ab577c6e39dbbee3fcd83dde5db290dbe
Udgeet <bhatt8udgeet@gmail.com>
2024-08-29 22:22:08 +0530

    Created Spinner

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { useConvexAuth } from "convex/react";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import  Link from "next/link";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -21,7 +23,7 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <Spinner />}
         {!isAuthenticated && !isLoading && (
           <>
             <SignInButton mode="modal">
@@ -32,6 +34,18 @@ export const Navbar = () => {
             <SignInButton mode="modal">
               <Button size="sm">Get Jotion Free</Button>
             </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">
+                Enter Jotion
+              </Link>
+            </Button>
+            <UserButton 
+              afterSignOutUrl="/"
+            />
           </>
         )}
         <ModeToggle />
